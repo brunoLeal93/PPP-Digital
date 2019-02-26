d7076ce4bfea5d122d4a9c56b59eb9af0a2b8233
Bruno Leal <bruno.carmo93@gmail.com>
2019-02-26 14:38:49 -0300

    Atualização

--- app/static/js/ppp.js
+++ app/static/js/ppp.js
@@ -1,3 +1,115 @@
+
+/* JS dos Checkbox */
+$('#checkAll').click(function(){
+    var chkAll = document.getElementById('checkAll');
+    var ckTrade = document.getElementById('trade');
+    var ckConjuntura  = document.getElementById('conjuntura');
+    var ckAAI  = document.getElementById('trade-aai');
+    var ckMercados  = document.getElementById('mercados');
+    var ckMercadosBS  = document.getElementById('mercados-bs');
+    var ckNews  = document.getElementById('news');
+    var ckNewsBS  = document.getElementById('news-bs');
+    var ckAnalises  = document.getElementById('analises');
+
+    if(chkAll.checked == true){
+
+        ckTrade.checked = true;
+        ckConjuntura.checked = true;
+        ckAAI.checked = true;
+        ckMercados.checked = true;
+        ckMercadosBS.checked = true;
+        ckNews.checked = true;
+        ckNewsBS.checked = true;
+        ckAnalises.checked = true;
+
+    } else {
+
+        ckTrade.checked = false;
+        ckConjuntura.checked = false;
+        ckAAI.checked = false;
+        ckMercados.checked = false;
+        ckMercadosBS.checked = false;
+        ckNews.checked = false;
+        ckNewsBS.checked = false;
+        ckAnalises.checked = false;
+    }
+});
+
+ $('#trade').click(function() {
+        
+        var ckTrade = document.getElementById('trade');
+        if(ckTrade.checked == false){
+            var chkAll = document.getElementById('checkAll');
+            chkAll.checked = false;
+        };
+
+    });
+    $('#trade-aai').click(function() {
+        
+        var ckTrade = document.getElementById('trade-aai');
+        if(ckTrade.checked == false){
+            var chkAll = document.getElementById('checkAll');
+            chkAll.checked = false;
+        };
+
+    });
+    $('#conjuntura').click(function() {
+        
+        var ckTrade = document.getElementById('conjuntura');
+        if(ckTrade.checked == false){
+            var chkAll = document.getElementById('checkAll');
+            chkAll.checked = false;
+        };
+
+    });
+    $('#mercados-bs').click(function() {
+        
+        var ckTrade = document.getElementById('mercados-bs');
+        if(ckTrade.checked == false){
+            var chkAll = document.getElementById('checkAll');
+            chkAll.checked = false;
+        };
+
+    });
+    $('#mercados').click(function() {
+        
+        var ckTrade = document.getElementById('mercados');
+        if(ckTrade.checked == false){
+            var chkAll = document.getElementById('checkAll');
+            chkAll.checked = false;
+        };
+
+    });
+    $('#news-bs').click(function() {
+        
+        var ckTrade = document.getElementById('news-bs');
+        if(ckTrade.checked == false){
+            var chkAll = document.getElementById('checkAll');
+            chkAll.checked = false;
+        };
+
+    });
+    $('#news').click(function() {
+        
+        var ckTrade = document.getElementById('news');
+        if(ckTrade.checked == false){
+            var chkAll = document.getElementById('checkAll');
+            chkAll.checked = false;
+        };
+
+    });
+    $('#analises').click(function() {
+         
+        var ckTrade = document.getElementById('analises');
+        if(ckTrade.checked == false){
+            var chkAll = document.getElementById('checkAll');
+            chkAll.checked = false;
+        };
+
+    });
+
+/* Funções para controlar movimentação dos icones da callopse*/
+
 function iconNoticia(){
     if($(document.getElementById("Noticias-collapse")).hasClass('show')){
         $('#iconN').removeClass('fa-angle-down');
@@ -101,200 +213,3 @@ function iconModNDJN(){
         $('#iconModNDJN').addClass('fa-angle-down');
     }
 }
-
-
-$(document).ready(function() {
-        $('.fixed').addClass('is-sticky');
-    
-
-    });
-    
-    
-    
-
-    $('#trade').click(function() {
-        $("#trade-lb").toggle(this.checked);
-        $("#n-trade").toggle(this.checked);
-        $("#n-aenews-trade").toggle(this.checked);
-        $("#n-empset-trade").toggle(this.checked);
-        $("#c-trade").toggle(this.checked);
-        $("#c-a-trade").toggle(this.checked);
-        $("#c-a-bov-trade").toggle(this.checked);
-        $("#c-a-nyse-trade").toggle(this.checked);
-        $("#c-a-nyseamex-trade").toggle(this.checked);
-        $("#c-d-trade").toggle(this.checked);
-        $("#c-d-cbot-trade").toggle(this.checked);
-        $("#f-trade").toggle(this.checked);
-        $("#f-chart-trade").toggle(this.checked);
-        $("#f-linkexcel-trade").toggle(this.checked);
-    });
-    $('#trade-aai').click(function() {
-        $("#trade-aai-lb").toggle(this.checked);
-        $("#n-trade-aai").toggle(this.checked);
-        $("#n-aenews-trade-aai").toggle(this.checked);
-        $("#n-empset-trade-aai").toggle(this.checked);
-        $("#c-trade-aai").toggle(this.checked);
-        $("#c-a-trade-aai").toggle(this.checked);
-        $("#c-a-bov-trade-aai").toggle(this.checked);
-        $("#c-a-nyse-trade-aai").toggle(this.checked);
-        $("#c-a-nyseamex-trade-aai").toggle(this.checked);
-        $("#c-d-trade-aai").toggle(this.checked);
-        $("#c-d-cbot-trade-aai").toggle(this.checked);
-        $("#f-trade-aai").toggle(this.checked);
-        $("#f-chart-trade-aai").toggle(this.checked);
-        $("#f-linkexcel-trade-aai").toggle(this.checked);
-    });
-    $('#conjuntura').click(function() {
-        $("#conjuntura-lb").toggle(this.checked);
-        $("#n-conjuntura").toggle(this.checked);
-        $("#n-aenews-conjuntura").toggle(this.checked);
-        $("#n-empset-conjuntura").toggle(this.checked);
-        $("#c-conjuntura").toggle(this.checked);
-        $("#c-a-conjuntura").toggle(this.checked);
-        $("#c-a-bov-conjuntura").toggle(this.checked);
-        $("#c-a-nyse-conjuntura").toggle(this.checked);
-        $("#c-a-nyseamex-conjuntura").toggle(this.checked);
-        $("#c-d-conjuntura").toggle(this.checked);
-        $("#c-d-cbot-conjuntura").toggle(this.checked);
-        $("#f-conjuntura").toggle(this.checked);
-        $("#f-chart-conjuntura").toggle(this.checked);
-        $("#f-linkexcel-conjuntura").toggle(this.checked);
-    });
-    $('#mercados-bs').click(function() {
-        $("#mercados-bs-lb").toggle(this.checked);
-        $("#n-mercados-bs").toggle(this.checked);
-        $("#n-aenews-mercados-bs").toggle(this.checked);
-        $("#n-empset-mercados-bs").toggle(this.checked);
-        $("#c-mercados-bs").toggle(this.checked);
-        $("#c-a-mercados-bs").toggle(this.checked);
-        $("#c-a-bov-mercados-bs").toggle(this.checked);
-        $("#c-a-nyse-mercados-bs").toggle(this.checked);
-        $("#c-a-nyseamex-mercados-bs").toggle(this.checked);
-        $("#c-d-mercados-bs").toggle(this.checked);
-        $("#c-d-cbot-mercados-bs").toggle(this.checked);
-        $("#f-mercados-bs").toggle(this.checked);
-        $("#f-chart-mercados-bs").toggle(this.checked);
-        $("#f-linkexcel-mercados-bs").toggle(this.checked);
-    });
-    $('#mercados').click(function() {
-        $("#mercados-lb").toggle(this.checked);
-        $("#n-mercados").toggle(this.checked);
-        $("#n-aenews-mercados").toggle(this.checked);
-        $("#n-empset-mercados").toggle(this.checked);
-        $("#c-mercados").toggle(this.checked);
-        $("#c-a-mercados").toggle(this.checked);
-        $("#c-a-bov-mercados").toggle(this.checked);
-        $("#c-a-nyse-mercados").toggle(this.checked);
-        $("#c-a-nyseamex-mercados").toggle(this.checked);
-        $("#c-d-mercados").toggle(this.checked);
-        $("#c-d-cbot-mercados").toggle(this.checked);
-        $("#f-mercados").toggle(this.checked);
-        $("#f-chart-mercados").toggle(this.checked);
-        $("#f-linkexcel-mercados").toggle(this.checked);
-    });
-    $('#news-bs').click(function() {
-        $("#news-bs-lb").toggle(this.checked);
-        $("#n-news-bs").toggle(this.checked);
-        $("#n-aenews-news-bs").toggle(this.checked);
-        $("#n-empset-news-bs").toggle(this.checked);
-        $("#c-news-bs").toggle(this.checked);
-        $("#c-a-news-bs").toggle(this.checked);
-        $("#c-a-bov-news-bs").toggle(this.checked);
-        $("#c-a-nyse-news-bs").toggle(this.checked);
-        $("#c-a-nyseamex-news-bs").toggle(this.checked);
-        $("#c-d-news-bs").toggle(this.checked);
-        $("#c-d-cbot-news-bs").toggle(this.checked);
-        $("#f-news-bs").toggle(this.checked);
-        $("#f-chart-news-bs").toggle(this.checked);
-        $("#f-linkexcel-news-bs").toggle(this.checked);
-    });
-    $('#news').click(function() {
-        $("#news-lb").toggle(this.checked);
-        $("#n-news").toggle(this.checked);
-        $("#n-aenews-news").toggle(this.checked);
-        $("#n-empset-news").toggle(this.checked);
-        $("#c-news").toggle(this.checked);
-        $("#c-a-news").toggle(this.checked);
-        $("#c-a-bov-news").toggle(this.checked);
-        $("#c-a-nyse-news").toggle(this.checked);
-        $("#c-a-nyseamex-news").toggle(this.checked);
-        $("#c-d-news").toggle(this.checked);
-        $("#c-d-cbot-news").toggle(this.checked);
-        $("#f-news").toggle(this.checked);
-        $("#f-chart-news").toggle(this.checked);
-        $("#f-linkexcel-news").toggle(this.checked);
-    });
-    $('#analises').click(function() {
-         $("#ad-analises").toggle(this.checked);
-         $("#ad-mod-af-analises").toggle(this.checked);
-         $("#ad-mod-af-capabe-analises").toggle(this.checked);
-         $("#ad-mod-af-capabefech-analises").toggle(this.checked);
-         $("#ad-mod-agro-analises").toggle(this.checked);
-         $("#ad-mod-analises").toggle(this.checked);
-         $("#ad-mod-pol-analises").toggle(this.checked);
-         $("#ad-n-analises").toggle(this.checked);
-         $("#ad-n-arko-analises").toggle(this.checked);
-         $("#ad-n-bdm-analises").toggle(this.checked);
-         $("#ad-n-cepea-analises").toggle(this.checked);
-         $("#ad-n-climatempo-analises").toggle(this.checked);
-         $("#ad-n-djn-analises").toggle(this.checked);
-         $("#ad-n-djn-cn-analises").toggle(this.checked);
-         $("#ad-n-djn-enn-analises").toggle(this.checked);
-         $("#ad-n-djn-eqn-analises").toggle(this.checked);
-         $("#ad-n-djn-fn-analises").toggle(this.checked);
-         $("#ad-n-nms-analises").toggle(this.checked);
-         $("#ad-n-npec-analises").toggle(this.checked);
-         $("#ad-n-pol-analises").toggle(this.checked);
-         $("#ad-n-tenon-analises").toggle(this.checked);
-         $("#analises-lb").toggle(this.checked);
-         $("#c-analises").toggle(this.checked);
-         $("#c-b3-bmf-analises").toggle(this.checked);
-         $("#c-b3-bov-analises").toggle(this.checked);
-         $("#c-cbot-analises").toggle(this.checked);
-         $("#c-cetip-analises").toggle(this.checked);
-         $("#c-cme-analises").toggle(this.checked);
-         $("#c-comex-analises").toggle(this.checked);
-         $("#c-ctbd-cambio-analises").toggle(this.checked);
-         $("#c-ctbd-cripto-analises").toggle(this.checked);
-         $("#c-ctbd-rfixa-analises").toggle(this.checked);
-         $("#c-forward-analises").toggle(this.checked);
-         $("#c-fundanbima-analises").toggle(this.checked);
-         $("#c-indcds-analises").toggle(this.checked);
-         $("#c-indcepea-analises").toggle(this.checked);
-         $("#c-indmundi-analises").toggle(this.checked);
-         $("#c-ipe-analises").toggle(this.checked);
-         $("#c-latibex-analises").toggle(this.checked);
-         $("#c-metais-analises").toggle(this.checked);
-         $("#c-moedasfull-analises").toggle(this.checked);
-         $("#c-moedasmini-analises").toggle(this.checked);
-         $("#c-nasdaq-analises").toggle(this.checked);
-         $("#c-nybot-analises").toggle(this.checked);
-         $("#c-nymex-analises").toggle(this.checked);
-         $("#c-nyse-analises").toggle(this.checked);
-         $("#c-taxasfull-analises").toggle(this.checked);
-         $("#c-taxasmini-analises").toggle(this.checked);
-         $("#c-titpubanbima-analises").toggle(this.checked);
-         $("#c-tresuaries-analises").toggle(this.checked);
-         $("#n-aenews-analises").toggle(this.checked);
-         $("#n-analises").toggle(this.checked);
-         $("#n-artigos-analises").toggle(this.checked);
-         $("#n-bctv-analises").toggle(this.checked);
-         $("#n-cadnewspaper-analises").toggle(this.checked);
-         $("#n-colunistas-analises").toggle(this.checked);
-         $("#n-conjfin-analises").toggle(this.checked);
-         $("#n-conjneg-analises").toggle(this.checked);
-         $("#n-ctbd-agbov-analises").toggle(this.checked);
-         $("#n-ctbd-agcebds-analises").toggle(this.checked);
-         $("#n-ctbd-agcnasenar-analises").toggle(this.checked);
-         $("#n-ctbd-agstj-analises").toggle(this.checked);
-         $("#n-ctbd-ceagesp-analises").toggle(this.checked);
-         $("#n-ctbd-ngo-analises").toggle(this.checked);
-         $("#n-ctbd-relagricolas-analises").toggle(this.checked);
-         $("#n-ctbd-relcriptomoedas-analises").toggle(this.checked);
-         $("#n-ctbd-relmacro-analises").toggle(this.checked);
-         $("#n-ctbd-relresearch-analises").toggle(this.checked);
-         $("#n-empset-analises").toggle(this.checked);
-         $("#n-projecoes-analises").toggle(this.checked);
-         $("#n-release-analises").toggle(this.checked);
-
-    });
